eee1bca43eef32cdcfa6cbf34a2ad25766b78603
Administrator <bahrom1982@gmail.com>
2024-07-29 15:18:36 +0500

    Commit

---
+++ src/JavaDemo.java
@@ -0,0 +1,85 @@
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class JavaDemo {
+    public interface TscLibDll extends Library {
+        TscLibDll INSTANCE = (TscLibDll) Native.loadLibrary("\\TSCLIB", TscLibDll.class);
+        int about();
+        int openport(String pirnterName);
+        int closeport();
+        int sendcommand(String printerCommand);
+        int setup(String width, String height, String speed, String density, String sensor, String vertical, String offset);
+        int downloadpcx(String filename, String image_name);
+        int barcode(String x, String y, String type, String height, String readable, String rotation, String narrow, String wide, String code);
+        int printerfont(String x, String y, String fonttype, String rotation, String xmul, String ymul, String text);
+        int clearbuffer();
+        int printlabel(String set, String copy);
+        int formfeed();
+        int nobackfeed();
+        int windowsfont(int x, int y, int fontheight, int rotation, int fontstyle, int fontunderline, String szFaceName, String content);
+    }
+
+    public  void print1(){
+        System.setProperty("jna.encoding", "GBK");// Support Chinese
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String time = df.format(new Date());
+        String qrCode = "PD102011";
+        TscLibDll.INSTANCE.openport("Xprinter XP-365B");//Printer model
+        TscLibDll.INSTANCE.setup("70","40","5","8","0","2","0");
+        TscLibDll.INSTANCE.clearbuffer();
+        String command = "QRCODE 20,90,Q,9,A,0,M1,S7,\"" + qrCode+"\""; //Print the parameters and contents of the QR code
+        TscLibDll.INSTANCE.sendcommand (command);
+        // TscLibDll.INSTANCE.sendcommand("TEXT 300 70 36 0 0 0 arial"+ "Office Supplies - Label Paper");
+        TscLibDll.INSTANCE.windowsfont(230, 120, 48, 0, 0, 0, "arial", "Product_Code");
+        TscLibDll.INSTANCE.windowsfont(230, 170, 68, 0, 0, 0, "arial", "6806");
+        TscLibDll.INSTANCE.printlabel("1", "1");
+        TscLibDll.INSTANCE.closeport();
+    }
+
+    public  void print2(){
+        System.setProperty("jna.encoding", "GBK");// Support Chinese
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String time = df.format(new Date());
+        String qrCode = "PD102011";
+        TscLibDll.INSTANCE.openport("Xprinter XP-365B");//Printer model
+        TscLibDll.INSTANCE.setup("30","20","5","8","0","2","0");
+        TscLibDll.INSTANCE.clearbuffer();
+        String command = "QRCODE 10,30,Q,5,A,0,M1,S7,\"" + qrCode+"\""; //Print the parameters and contents of the QR code
+        TscLibDll.INSTANCE.sendcommand (command);
+        // TscLibDll.INSTANCE.sendcommand("TEXT 300 70 36 0 0 0 arial"+ "Office Supplies - Label Paper");
+        TscLibDll.INSTANCE.windowsfont(130, 25, 38, 0, 0, 0, "arial", "12$");
+        TscLibDll.INSTANCE.windowsfont(130, 60, 38, 0, 0, 0, "arial", "Code");
+        TscLibDll.INSTANCE.windowsfont(130, 100, 38, 0, 0, 0, "arial", "6806");
+        TscLibDll.INSTANCE.printlabel("1", "1");
+        TscLibDll.INSTANCE.closeport();
+    }
+
+    public  void print3(){
+        System.setProperty("jna.encoding", "GBK");// Support Chinese
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String time = df.format(new Date());
+        String qrCode = "PD102011";
+        TscLibDll.INSTANCE.openport("Xprinter XP-365B");//Printer model
+        TscLibDll.INSTANCE.setup("40","30","5","8","0","2","0");
+        TscLibDll.INSTANCE.clearbuffer();
+        String command = "QRCODE 15,40,Q,7,A,0,M1,S7,\"" + qrCode+"\""; //Print the parameters and contents of the QR code
+        TscLibDll.INSTANCE.sendcommand (command);
+        // TscLibDll.INSTANCE.sendcommand("TEXT 300 70 36 0 0 0 arial"+ "Office Supplies - Label Paper");
+        TscLibDll.INSTANCE.windowsfont(170, 25, 55, 0, 0, 0, "arial", "12$");
+        TscLibDll.INSTANCE.windowsfont(170, 80, 55, 0, 0, 0, "arial", "Code");
+        TscLibDll.INSTANCE.windowsfont(170, 135, 55, 0, 0, 0, "arial", "6806");
+        TscLibDll.INSTANCE.printlabel("1", "1");
+        TscLibDll.INSTANCE.closeport();
+    }
+
+    public static void main(String[] args) {
+        new JavaDemo().print3();
+    }
+
+
+
+}
---
+++ src/JavaTest.java
@@ -0,0 +1,78 @@
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+public class JavaTest 
+{
+
+	public interface TscLibDll extends Library
+	{		
+		TscLibDll INSTANCE = (TscLibDll) Native.loadLibrary("\\TSCLIB", TscLibDll.class);
+        int about ();
+        int openport (String pirnterName);
+        int closeport ();
+        int sendcommand (String printerCommand);
+        int sendBinaryData (byte[] printerCommand, int CommandLength);
+        int setup (String width,String height,String speed,String density,String sensor,String vertical,String offset);
+        int downloadpcx (String filename,String image_name);
+        int barcode (String x,String y,String type,String height,String readable,String rotation,String narrow,String wide,String code);
+        int qrCode (String x,String y,String type,String height,String readable,String rotation,String narrow,String wide,String code);
+        int printerfont (String x,String y,String fonttype,String rotation,String xmul,String ymul,String text);
+        int clearbuffer ();
+        int printlabel (String set, String copy);        
+        int windowsfont (int x, int y, int fontheight, int rotation, int fontstyle, int fontunderline, String szFaceName, String content);
+        int windowsfontUnicode(int x, int y, int fontheight, int rotation, int fontstyle, int fontunderline, String szFaceName, byte[] content);
+        int windowsfontUnicodeLengh(int x, int y, int fontheight, int rotation, int fontstyle, int fontunderline, String szFaceName, byte[] content, int length);
+        byte usbportqueryprinter();
+        
+	}
+    public static void main(String[] args)
+    {
+
+        String WT1 = "Xprinter XP 365B";
+        String B1 = "20080101";
+
+    	//unicode format
+    	byte[] result_unicode = new byte[1024];
+    	String word_unicode = "Xprinter XP 365B";
+    	result_unicode = word_unicode.getBytes(StandardCharsets.UTF_16LE);
+
+    	//utf-8 format
+    	byte[] result_utf8 = new byte[1024];
+    	String word_utf8 = "TEXT 40,620,\"ARIAL.TTF\",0,12,12,\"utf8 test Wörter auf Deutsch\"";
+    	result_utf8 = word_utf8.getBytes(StandardCharsets.UTF_8);
+
+    	
+        //TSCLIB_DLL.about();
+        byte status = TscLibDll.INSTANCE.usbportqueryprinter();//0 = idle, 1 = head open, 16 = pause, following <ESC>!? command of TSPL manual
+        TscLibDll.INSTANCE.openport("Xprinter XP-365B");
+        TscLibDll.INSTANCE.sendcommand("SIZE 40 mm, 39.5 mm");
+        TscLibDll.INSTANCE.sendcommand("SPEED 4");
+        TscLibDll.INSTANCE.sendcommand("DENSITY 12");
+        TscLibDll.INSTANCE.sendcommand("DIRECTION 0");
+        TscLibDll.INSTANCE.sendcommand("SET TEAR ON");
+        TscLibDll.INSTANCE.sendcommand("CODEPAGE UTF-8");
+        TscLibDll.INSTANCE.clearbuffer();
+//        TscLibDll.INSTANCE.downloadpcx("\\UL.PCX", "UL.PCX");
+        TscLibDll.INSTANCE.windowsfont(40, 70 , 48, 0, 0, 0, "Arial", "Windows Font Test");
+        TscLibDll.INSTANCE.windowsfontUnicodeLengh(40, 50, 30, 0, 0, 0, "Arial", result_unicode,word_unicode.length());
+        TscLibDll.INSTANCE.sendcommand("PUTPCX 40,40,\"UL.PCX\"");
+        TscLibDll.INSTANCE.sendBinaryData(result_utf8, result_utf8.length);
+        TscLibDll.INSTANCE.barcode("40", "100", "128", "80", "1", "0", "2", "2", B1);
+        //TscLibDll.INSTANCE.qrcode("40", "100", "128", "80", "1", "0", "2", "2", B1);
+        TscLibDll.INSTANCE.printerfont("40", "440", "0", "0", "15", "15", WT1);
+        TscLibDll.INSTANCE.printlabel("1", "1");
+        TscLibDll.INSTANCE.closeport();
+    	
+    	
+    	
+    	
+    	
+    }
+    
+
+}
+
+
